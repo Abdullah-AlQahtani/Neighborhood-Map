9072782fbcc2622f031b2da37a4421be6366e702
BuildTools <unconfigured@null.spigotmc.org>
2018-07-10 23:31:21 +0300

    Update commit

--- src/App.js
+++ src/App.js
@@ -123,7 +123,7 @@ class App extends Component {
         let newData = [...this.state.data,[responseJson,responseJson[2][0],responseJson[3][0]]]
         this.updateData(newData)
       }).catch(error =>
-      console.error(error)      
+      console.log(error)      
       )
     })
   }
